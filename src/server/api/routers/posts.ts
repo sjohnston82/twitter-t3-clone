00591acdb242e27831ec1b5386588376b20e294c
sjohnston82 <72237516+sjohnston82@users.noreply.github.com>
2023-04-04 17:23:41 -0400

    added single post page

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -105,4 +105,18 @@ export const postsRouter = createTRPCRouter({
         })
         .then(addUserDataToPosts)
     ),
+
+  getPostByPostId: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const post = await ctx.prisma.post.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+
+      if (!post) throw new TRPCError({ code: "NOT_FOUND" });
+
+      return (await addUserDataToPosts([post]))[0];
+    }),
 });
